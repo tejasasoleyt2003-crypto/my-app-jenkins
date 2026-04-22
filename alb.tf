a98493dd1eabc578bc4ee4f5ba0402945f966350
tejasasoleyt2003-crypto <tejasasoleyt2003@gmail.com>
2026-04-22 17:13:51 +0530

    Create alb.tf

---
+++ alb.tf
@@ -0,0 +1,27 @@
+resource "aws_lb" "alb" {
+  name = "app-alb"
+  load_balancer_type = "application"
+
+  subnets = [
+    aws_subnet.public_1.id,
+    aws_subnet.public_2.id
+  ]
+
+  security_groups = [aws_security_group.alb_sg.id]
+}
+
+resource "aws_lb_target_group" "tg" {
+  port     = 3000
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
